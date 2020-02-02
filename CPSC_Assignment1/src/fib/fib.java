736e5b5c672866fba2071d76da9e06281805eb5a
Garth Slaney <45348251+GarthS1@users.noreply.github.com>
2020-02-02 13:24:44 -0700

    Update fib.java

--- CPSC_Assignment1/src/fib/fib.java
+++ CPSC_Assignment1/src/fib/fib.java
@@ -7,9 +7,10 @@ package fib;
  */
 public class fib {
 	public static void main(String[] args) {
+		
 		// for loop to test all five algorithms 
 		// cycles through each one to avoid processor disruptions 
-		for(int i = 0; i < 100; i++)
+		for(int i = 0; i <= 50; i++) 
 		{
 			//arrays to store times 
 			long rec[] = new long [5];
@@ -28,12 +29,13 @@ public class fib {
 			// runs each index five times and averages it in an array 
 			for(int j = 0; j < 5; j++)
 			{
+
 				// getting time for Alg.1
 				long timePast = System.nanoTime();
 				long num = fibRec(i);
 				long timeTaken = System.nanoTime() - timePast;
 				rec[j] = timeTaken;
-
+				
 				// getting time for Alg.2
 			  timePast = System.nanoTime();
 				num = fibMem(i);
@@ -83,7 +85,128 @@ public class fib {
 			System.out.println(mem_average + " time to compute F" + i + " with alg.2");
 			System.out.println(dyn_average + " time to compute F" + i + " with alg.3");
 			System.out.println(itr_average + " time to compute F" + i + " with alg.4");
-			System.out.println(mat_average + " time to compute F" + i + " with alg.5");
+			System.out.println(mat_average + " time to compute F" + i + " with alg.5\n");
+		}
+		
+		//going up by powers of 2 for alg.2-5 stops at 16384 otherwise encounters stack overflow in alg.2 
+		for(int i = 2; i <= 16384; i *=2)
+		{
+			//arrays to store times 
+			long mem[] = new long [5];
+			long dyn[] = new long [5];
+			long itr[] = new long [5];
+			long mat[] = new long [5];
+			
+			//average time for each process 
+			long mem_average = 0;
+			long dyn_average = 0;
+			long itr_average = 0;
+			long mat_average = 0;
+			
+			// runs each index five times and averages it in an array 
+			for(int j = 0; j < 5; j++)
+			{
+				
+				// getting time for Alg.2
+			  long timePast = System.nanoTime();
+				long num = fibMem(i);
+				long timeTaken = System.nanoTime()- timePast;
+				mem[j] = timeTaken;
+					
+				//getting time for Alg. 3
+			  timePast = System.nanoTime();
+				num = fibDyn(i);
+				timeTaken = System.nanoTime()- timePast;
+				dyn[j] = timeTaken;
+				
+				//getting time for Alg. 4
+			  timePast = System.nanoTime();
+				num = fibItr(i);
+				timeTaken = System.nanoTime()- timePast;
+				itr[j] = timeTaken;
+				
+				//getting time for Alg. 5
+			  timePast = System.nanoTime();
+				num = fibMat(i);
+				timeTaken = System.nanoTime()- timePast;
+				mat[j] = timeTaken;
+			}
+			
+			//sum up the arrays 		
+			for(int a = 0; a < 5; a++)
+				mem_average += mem[a];			
+			for(int a = 0; a < 5; a++)
+				dyn_average += dyn[a];
+			for(int a = 0; a < 5; a++)
+				itr_average += itr[a];
+			for(int a = 0; a < 5; a++)
+				mat_average += mat[a];
+			
+			//caculate the average time 
+			mem_average /= 5;
+			dyn_average /= 5;
+			itr_average /= 5;
+			mat_average /= 5;
+			
+			//print out times taken
+			System.out.println(mem_average + " time to compute F" + i + " with alg.2");
+			System.out.println(dyn_average + " time to compute F" + i + " with alg.3");
+			System.out.println(itr_average + " time to compute F" + i + " with alg.4");
+			System.out.println(mat_average + " time to compute F" + i + " with alg.5\n");
+		}
+		
+		//going up by powers of 2 for alg.3-5 
+		for(int i = 2; i <= 134217728; i *=2)
+		{
+			//arrays to store times 
+			long dyn[] = new long [5];
+			long itr[] = new long [5];
+			long mat[] = new long [5];
+			
+			//average time for each process 
+			long dyn_average = 0;
+			long itr_average = 0;
+			long mat_average = 0;
+			
+			// runs each index five times and averages it in an array 
+			for(int j = 0; j < 5; j++)
+			{
+				//getting time for Alg. 3
+			  long timePast = System.nanoTime();
+				long num = fibDyn(i);
+				long timeTaken = System.nanoTime()- timePast;
+				dyn[j] = timeTaken;
+				
+				//getting time for Alg. 4
+			  timePast = System.nanoTime();
+				num = fibItr(i);
+				timeTaken = System.nanoTime()- timePast;
+				itr[j] = timeTaken;
+				
+				//getting time for Alg. 5
+			  timePast = System.nanoTime();
+				num = fibMat(i);
+				timeTaken = System.nanoTime()- timePast;
+				mat[j] = timeTaken;
+			}
+			
+			//sum up the arrays 				
+			for(int a = 0; a < 5; a++)
+				dyn_average += dyn[a];
+			for(int a = 0; a < 5; a++)
+				itr_average += itr[a];
+			for(int a = 0; a < 5; a++)
+				mat_average += mat[a];
+			
+			//caculate the average time 
+			dyn_average /= 5;
+			itr_average /= 5;
+			mat_average /= 5;
+			
+			//print out times taken
+			System.out.println(dyn_average + " time to compute F" + i + " with alg.3");
+			System.out.println(itr_average + " time to compute F" + i + " with alg.4");
+			System.out.println(mat_average + " time to compute F" + i + " with alg.5\n");
 		}
 	}	
 
@@ -113,8 +236,8 @@ public class fib {
 	public static long fibMem(int n) {
 		
 		//makes an array to store previous recursive calls
-		long arr[] = new long[100];
-		for(int i = 0; i < 100; i++)
+		long arr[] = new long[n + 1];
+		for(int i = 0; i < n + 1; i++)
 			arr[i] = -1;
 		
 		return cache(n, arr);	
